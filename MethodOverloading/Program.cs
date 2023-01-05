b8acc091640397b2f6fde3c658e54e10f1faa642
Rhea Epperson <re.epperson2@gmail.com>
2023-01-05 11:07:14 -0500

    finished exercise

--- MethodOverloading/Program.cs
+++ MethodOverloading/Program.cs
@@ -4,7 +4,43 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Console.WriteLine(Add(8, 4));
+
+            //Console.WriteLine($"Decimal verson: {Add(4.3m, 9.7m)}");
+
+            Console.WriteLine(Add(15, 12, false));
+
+            Console.WriteLine(Add(15, 12, true));
+
+            Console.WriteLine(Add(1, 0, true));
+        }
+
+        public static int Add(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+        public static decimal Add(decimal num1, decimal num2)
+        {
+            return num1 + num2;
+        }
+
+        public static string Add(int num1, int num2, bool isTrue)
+        {
+            var sum = num1 + num2;
+
+            if (isTrue && sum > 1)
+            {
+                return $"{sum} dollars";
+            }
+            else if (isTrue && sum == 1)
+            {
+                return $"{sum} dollar";
+            }
+            else
+            {
+                return sum.ToString();
+            }
         }
     }
 }
